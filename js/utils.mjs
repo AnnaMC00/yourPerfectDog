5d819349cc80171f58a1628b49cc205bba0897c2
AnnaMC00 <am.casariegoc00@gmail.com>
2023-10-25 07:42:32 -0300

    Fixing Bugs #5

--- js/utils.mjs
+++ js/utils.mjs
@@ -44,8 +44,8 @@ export async function loadTemplate(path) {
 }
 
 export async function loadHeaderFooter() {
-    const headerTemplate = await loadTemplate("/partials/header.html");
-    const footerTemplate = await loadTemplate("/partials/footer.html");
+    const headerTemplate = await loadTemplate("/yourPerfectDog/partials/header.html");
+    const footerTemplate = await loadTemplate("/yourPerfectDog/partials/footer.html");
     const headerElement = document.getElementById("main-header");
     const footerElement = document.getElementById("main-footer");
     renderWithTemplate(headerTemplate, headerElement);
