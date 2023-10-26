5295815ab13a9b0d62fd5e49a0ed10f88c9d71c2
AnnaMC00 <am.casariegoc00@gmail.com>
2023-10-26 20:27:51 -0300

    Dog Page and Fav Page with CSS

--- js/dog-page.js
+++ js/dog-page.js
@@ -6,9 +6,12 @@ loadHeaderFooter();
 const data = new ExternalServices();
 
 function dogTemplate(dog) {
-    return `<h3>${dog.name}</h3>
-            <img src="${dog.image_link}" alt="${dog.name}"/>
-            <button id="fav-button">♡</button>`;
+    return `<div><img src="${dog.image_link}" alt="${dog.name}"/></div>
+            <div><h2>${dog.name}<button id="fav-button"><i class='bx bx-heart'></i></button><br></h2>
+            <p>Energy Level: <span>${dog.energy}</span></p>
+            <p>Barking Level: <span>${dog.barking}</span></p>
+            <p>Shedding Level: <span>${dog.shedding}</span></p>
+            <p>Trainability Level: <span>${dog.trainability}</span></p></div>`;
 
 }
 
@@ -31,7 +34,13 @@ async function renderDog() {
     document.querySelector("#dog-container").innerHTML = "";
     document.querySelector("#dog-container").innerHTML = dogTemplate(dogData);
 
-    document.getElementById("fav-button").addEventListener("click", addToFav.bind(this));
+    const title = document.querySelector("title");
+    title.textContent = dogData.name + title.textContent;
+
+    document.getElementById("fav-button").addEventListener("click", () => {
+        addToFav.bind(this)
+        document.querySelector(".bx-heart").classList.replace('bx-heart', 'bxs-heart')
+    });
 }
 
 renderDog();
--- js/fav-page.js
+++ js/fav-page.js
@@ -6,7 +6,7 @@ function favDogTemplate(dog) {
   const newDog = `<li>
     <a href="/dog-page/index.html?name=${dog.name}">
       <img src="${dog.image_link} alt="${dog.name}"/>
-      <h2>${dog.name}</h2>
+      <h3>${dog.name}</h3>
     </a>
   </li>`;
   return newDog;
